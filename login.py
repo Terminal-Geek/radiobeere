68b49c4eb6253d4a4cc27d6da1100744c4cfd96a
Terminal-Geek <github@tobias-gehle.de>
2015-09-27 23:20:04 +0200

    Passwörter geschützt

--- login.py
+++
@@ -1,5 +0,0 @@
-global DB_HOST
-DB_HOST = 'localhost'
-DB_USER = 'root'
-DB_PASSWORD = 'test'
-DB_DATABASE = 'radiobeere'
--- var/www/include/db-connect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$verbindung = mysql_connect("localhost","pi","test");
-mysql_select_db("radiobeere");
-?>
