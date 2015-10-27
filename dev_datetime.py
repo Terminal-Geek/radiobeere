6a29fef74ae3b9ae531c3366831dd0b3c6823393
Terminal-Geek <post@tobias-gehle.de>
2015-10-27 20:48:30 +0100

    Optimierung rb-rec-add.py

--- dev_datetime.py
+++ dev_datetime.py
@@ -12,28 +12,26 @@ from mutagen.id3 import ID3, TIT2, TPE1, TALB
 import datetime
 
 
-filename_pattern = '/var/www/Aufnahmen/aufnahme_fertig_*.mp3'
-date_time_format = '%Y_%m_%d_%H_%M_%S'
+FILENAME_PATTERN = '/var/www/Aufnahmen/aufnahme_fertig_*.mp3'
+DATE_TIME_FORMAT = '%Y_%m_%d_%H_%M_%S'
 
 def audio_length(filename):
 
-    length = str(datetime.timedelta(seconds = int((MP3(filename)).info.length)))
+    length = str(
+        datetime.timedelta(seconds = int((MP3(filename)).info.length))
+        )
 
     return length
 
 
 def extract_metadata(filename):
 
-    _, _, station_alias, date_time = filename.split('_',3)    
-    date_time = datetime.datetime.strptime(date_time, date_time_format)
-    title = '{0}, {1:%d.%m.%Y, %H:%M} Uhr'.format(station_alias, date_time)
-    date = '{0:%Y-%m-%d}'.format(date_time)
-    time = '{0:%H:%M}'.format(date_time)
-    print(title)
-    print(date)
-    print(time)
+    _, _, station_alias, recording_time = filename.split('_',3)    
+    recording_time = (
+        datetime.datetime.strptime(recording_time, DATE_TIME_FORMAT)
+        )
 
-    return station_alias, date_time
+    return station_alias, recording_time
 
 
 def get_station_name(connection, station_alias):
@@ -48,14 +46,20 @@ def get_station_name(connection, station_alias):
         return row[0]
 
 
-def id3_tag(path, title, artist, album):
+def id3_tag(path, station, recording_time):
 
     audio = ID3()
     audio.save(path)
     audio = ID3(path)
-    audio.add(TIT2(encoding=3, text = u"%s" % title))
-    audio.add(TPE1(encoding=3, text = u"%s" % artist))
-    audio.add(TALB(encoding=3, text = u"%s" % album))
+    audio.add(
+        TIT2(
+            encoding=3, text = '{0}, {1:%d.%m.%Y, %H:%M} Uhr'.format(
+            station, recording_time
+            )
+            )
+        )
+    audio.add(TPE1(encoding=3, text = station))
+    audio.add(TALB(encoding=3, text = '{0:%Y-%m-%d}'.format(recording_time)))
     audio.save(v2_version=3)
 
 
@@ -74,15 +78,20 @@ def main():
     (MySQLdb.connect(login.DB_HOST, login.DB_USER, login.DB_PASSWORD, login.DB_DATABASE)) \
     as connection:
 
-        for path in glob(filename_pattern):
+        for path in glob(FILENAME_PATTERN):
 
             directory = os.path.dirname(path)
             filename, extension = os.path.splitext(os.path.basename(path))
 
             length = audio_length(path)
-            extract_metadata(filename)
-##            station = get_station_name(connection, station_alias)
+            station_alias, recording_time = extract_metadata(filename)
+            station = get_station_name(connection, station_alias)
 
+
+            title = '{0}, {1:%d.%m.%Y, %H:%M} Uhr'.format(station_alias, recording_time)
+            date = '{0:%Y-%m-%d}'.format(recording_time)
+            time = '{0:%H:%M}'.format(recording_time)
+       
 ##            date = '{0}-{1}-{2}'.format(year, month, day)
 ##            time = '{0}:{1}'.format(hour, minutes)
 ##            title = '{0}, {1}.{2}.{3}, {4} Uhr'.format(station, day, month, year, time)
@@ -90,7 +99,7 @@ def main():
 ##            new_filename = '{0}_{1}-{2}-{3}_{4}-{5}{6}'.format \
 ##            (station_alias, year, month, day, hour, minutes, extension)
 ##
-##            id3_tag(path, title, station, date)
+            id3_tag(path, station, recording_time)
 ##            write_to_db(connection, date, time, station, new_filename, timestamp, length)
 ##            os.rename(path, (os.path.join(directory, new_filename)))
 
