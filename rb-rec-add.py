97284edeca4da5c2c6e28ea96c1624d43dfc8959
Terminal-Geek <github@tobias-gehle.de>
2015-10-29 21:08:13 +0100

    Neue Version rb-rec-add.py

--- rb-rec-add.py
+++ rb-rec-add.py
@@ -12,28 +12,36 @@ from mutagen.id3 import ID3, TIT2, TPE1, TALB
 import datetime
 
 
-filename_pattern = '/var/www/Aufnahmen/aufnahme_fertig_*.mp3'
-
+FILENAME_PATTERN = '/var/www/Aufnahmen/aufnahme_fertig_*.mp3'
+DATE_TIME_FORMAT = '%Y_%m_%d_%H_%M_%S'
 
 def audio_length(filename):
 
-    length = str(datetime.timedelta(seconds = int((MP3(filename)).info.length)))
+    length = str(
+        datetime.timedelta(seconds = int((MP3(filename)).info.length))
+        )
 
     return length
 
 
 def extract_metadata(filename):
 
-    _, _, station_alias, year, month, day, hour, minutes, _ = filename.split('_')
-    timestamp = int(time.mktime((int(year), int(month), int(day), int(hour), int(minutes), 0, 0, 0, -1)))
+    _, _, station_alias, recording_time = filename.split('_',3)    
+    recording_time = (
+            datetime.datetime.strptime(
+            recording_time, DATE_TIME_FORMAT)
+    )
 
-    return station_alias, year, month, day, hour, minutes, timestamp
+    return station_alias, recording_time
 
 
 def get_station_name(connection, station_alias):
 
     with closing(connection.cursor()) as cursor:
-        cursor.execute('SELECT name FROM sender WHERE alias=%s', (station_alias,))
+        cursor.execute(
+                'SELECT name FROM sender WHERE alias=%s', (
+                station_alias,)
+        )
         row = cursor.fetchone()
 
         if not row:
@@ -42,50 +50,58 @@ def get_station_name(connection, station_alias):
         return row[0]
 
 
-def id3_tag(path, title, artist, album):
+def id3_tag(path, station, recording_time):
 
     audio = ID3()
     audio.save(path)
     audio = ID3(path)
-    audio.add(TIT2(encoding=3, text = u"%s" % title))
-    audio.add(TPE1(encoding=3, text = u"%s" % artist))
-    audio.add(TALB(encoding=3, text = u"%s" % album))
+    audio.add(TIT2(
+            encoding=3, text = '{0}, {1:%d.%m.%Y, %H:%M} Uhr'.format(
+            station, recording_time))
+    )
+    audio.add(TPE1(encoding=3, text = station))
+    audio.add(TALB(encoding=3, text = '{0:%Y-%m-%d}'.format(recording_time)))
     audio.save(v2_version=3)
 
 
-def write_to_db(connection, date, time, station, new_filename, timestamp, length):
+def write_to_db(connection, recording_time, station, new_filename, length):
+
+    date = '{0:%Y-%m-%d}'.format(recording_time)
+    time = '{0:%H:%M}'.format(recording_time)
+    timestamp = int(recording_time.strftime("%s"))
 
     with closing(connection.cursor()) as cursor:
 
-        cursor.execute('INSERT INTO aufnahmen(datum, uhrzeit, sender, datei, zeitstempel, laenge) \
-        VALUES (%s,%s,%s,%s,%s,%s)',(date, time, station, new_filename, timestamp, length,))
+        cursor.execute('INSERT INTO aufnahmen \
+        (datum, uhrzeit, sender, datei, zeitstempel, laenge) \
+        VALUES (%s,%s,%s,%s,%s,%s)', \
+        (date, time, station, new_filename, timestamp, length,))
+
         connection.commit()
 
 
 def main():
 
-    with closing \
-    (MySQLdb.connect(login.DB_HOST, login.DB_USER, login.DB_PASSWORD, login.DB_DATABASE)) \
-    as connection:
+    with closing (MySQLdb.connect(
+            login.DB_HOST, login.DB_USER,
+            login.DB_PASSWORD, login.DB_DATABASE)) as connection:
 
-        for path in glob(filename_pattern):
+        for path in glob(FILENAME_PATTERN):
 
             directory = os.path.dirname(path)
             filename, extension = os.path.splitext(os.path.basename(path))
 
             length = audio_length(path)
-            station_alias, year, month, day, hour, minutes, timestamp = extract_metadata(filename)
+            station_alias, recording_time = extract_metadata(filename)
             station = get_station_name(connection, station_alias)
+            new_filename = '{0}_{1:%Y-%m-%d}_{1:%H-%M}{2}'.format(
+                    station_alias, recording_time, extension
+            )
 
-            date = '{0}-{1}-{2}'.format(year, month, day)
-            time = '{0}:{1}'.format(hour, minutes)
-            title = '{0}, {1}.{2}.{3}, {4} Uhr'.format(station, day, month, year, time)
-
-            new_filename = '{0}_{1}-{2}-{3}_{4}-{5}{6}'.format \
-            (station_alias, year, month, day, hour, minutes, extension)
-
-            id3_tag(path, title, station, date)
-            write_to_db(connection, date, time, station, new_filename, timestamp, length)
+            id3_tag(path, station, recording_time)
+            write_to_db(
+                    connection, recording_time, station, new_filename, length
+            )
             os.rename(path, (os.path.join(directory, new_filename)))
 
 if __name__ == '__main__':
