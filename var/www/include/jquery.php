905967af34117040d525df053b608d05c41252d1
Terminal-Geek <github@tobias-gehle.de>
2015-11-14 00:22:38 +0100

    JQuery - Ajax deaktiviert

--- var/www/include/jquery.php
+++ var/www/include/jquery.php
@@ -1,11 +1,9 @@
         <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+			<script type="text/javascript">
+      			$(document).bind("mobileinit", function(){
+            		$.extend(  $.mobile , { 
+            			ajaxEnabled : false
+             	});
+      			});
+			</script>
         <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-		 <script type='text/javascript'>
-                <!--
-                $(document).bind('mobileinit',function(){
-                $.extend(  $.mobile , {
-                defaultPageTransition: "none"
-                });
-                });
-                //-->
-                </script>
