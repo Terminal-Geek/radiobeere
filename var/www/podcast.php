2d485bced3c9743eab52a81538d6989b782b5764
Terminal-Geek <github@tobias-gehle.de>
2015-11-21 23:55:25 +0100

    Update podcast

--- var/www/podcast.php
+++ var/www/podcast.php
@@ -1,71 +1,71 @@
 <!DOCTYPE html>
-<html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-        <title>RadioBeere - Podcast</title>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="utf-8">
-
-	<?php
-	include("include/styling.php");
-	?>
+    <title>RadioBeere - Podcast</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <?php
+    include("include/styling.php");
+    ?>
 </head>
 
 <body>
-
-<div data-role="page" class="ui-responsive-panel" id="panel" data-title="RadioBeere">
+    <div data-role="page"
+         class="ui-responsive-panel"
+         id="panel"
+         data-title="RadioBeere">
 
         <div data-role="header">
-                <a href="#nav-panel" data-icon="bars" data-iconpos="notext">Men&uuml;</a>
-                <h1>RadioBeere</h1>
-                <a href="/" data-icon="home" data-iconpos="notext">Startseite</a>
+            <a href="#nav-panel"
+                 data-icon="bars"
+                 data-iconpos="notext">Menü</a>
+            <h1>RadioBeere</h1>
+            <a href="/"
+                 data-icon="home"
+                 data-iconpos="notext">Startseite</a>
         </div>
 
-<?php
+        <?php
         include("include/db-connect.php")
-?>
+        ?>
 
-        <div data-role="main" class="ui-content">
-	<h2>Podcast</h2>
+        <div data-role="main"
+             class="ui-content">
+            <h2>Podcast</h2>
 
-	<p>Abonniere deine eigenen Radio-Aufnahmen als Podcast. Sollte ein Klick
-	auf den jeweiligen Link nicht automatisch dein Podcast-Programm öffnen,
-	kopiere den Link und füge ihn in deinem Podcast-Programm von Hand ein.
-	In iTunes zum Beispiel gehst du dafür in der Podcast-Ansicht auf
-	<b>Ablage - > Podcast abonnieren</b>.</p>
+            <p>Abonniere deine eigenen Radio-Aufnahmen als Podcast. Sollte ein
+            Klick auf den jeweiligen Link nicht automatisch dein
+            Podcast-Programm öffnen, kopiere den Link und füge ihn in deinem
+            Podcast-Programm von Hand ein. In iTunes zum Beispiel gehst du
+            dafür in der Podcast-Ansicht auf <b>Ablage - &gt; Podcast
+            abonnieren</b>.</p>
 
-	<p>
-<?php
-		$hostname = gethostname();
-	  	$abfrage = "SELECT * FROM sender ORDER BY name";
-	  	$ergebnis = mysql_query($abfrage);
-	 	while($row = mysql_fetch_object($ergebnis))
-		 {
-		 	$feed = "$hostname/podcast/$row->alias.xml";
-		 echo "<b>$row->name:</b><br><a href=\"pcast://$feed\" target=\"_blank\">http://$feed</a><br><br>";
-		}
-?>
-	</p>
+            <p>
+            <?php
+            $hostname = gethostname();
+            $abfrage = "SELECT * FROM sender ORDER BY name";
+            $ergebnis = mysql_query($abfrage);
+            while($row = mysql_fetch_object($ergebnis))
+                 {
+                $feed = "http://$hostname/podcast/$row->alias.xml";
+                echo "<b>$row->name:</b><br><a href=\"$feed\" target=\"_blank\">$feed</a><br><br>";
+                }
+            ?>
+            </p>
 
-
-       <div class="illu-content-wrapper">
-        <div class="illu-content illu-podcast">
-        </div></div>
-
-	</div>
-
-<!--- Navigation --->
-
+            <div class="illu-content-wrapper">
+                <div class="illu-content illu-podcast">
+                </div>
+            </div>
+        </div>
         <?php
         include("include/navigation.php");
         ?>
-
-</div>
-
-        <?php
-        include("include/jquery.php");
-        ?>
-
+    </div>
+    <?php
+    include("include/jquery.php");
+    ?>
 </body>
-
 </html>
