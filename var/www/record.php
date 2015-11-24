2302cdcb950df83ba372a10dc9703a2b585da98a
Terminal-Geek <github@tobias-gehle.de>
2015-11-24 22:40:45 +0100

    Cleaned up html

--- var/www/record.php
+++ var/www/record.php
@@ -1,103 +1,95 @@
 <!DOCTYPE html>
-<html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-        <title>RadioBeere - Aufnahmen planen</title>
-
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta charset="utf-8">
-
-	<?php
-	include("include/styling.php");
-	?>
+    <title>RadioBeere - Aufnahmen planen</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <?php
+    include("include/styling.php");
+    ?>
 </head>
 
 <body>
 
-<!--- Verbindung zur Datenbank aufbauen --->
+    <?php
+    include("include/db-connect.php");
+    ?>
 
-	<?php
-	include("include/db-connect.php");
-	?>
+<!-- Delete latest timer -->
 
-<!--- Variablen abrufen --->
-
-        <?php
-        $reset = $_POST["reset"];
-	?>
-
-<!--- Letzten Timer löschen --->
-
-	<?php
-        if ($reset == "1")
+    <?php
+    $reset = $_POST["reset"];
+    if ($reset == "1")
         {
-	$abfrage = "SELECT id FROM timer ORDER BY id DESC LIMIT 1";
-	$ergebnis = mysql_query($abfrage);
-	while($row = mysql_fetch_object($ergebnis))
-   	{
-	$id =("$row->id");
-   	}
-	$loeschen = "DELETE FROM timer WHERE id = '$id'";
-	$loesch = mysql_query($loeschen);
-	$reset = "0";
-	}
-	exec("sudo /home/pi/radiobeere/rb-timer-update.py");
-	?>
+        $abfrage = "SELECT id FROM timer ORDER BY id DESC LIMIT 1";
+        $ergebnis = mysql_query($abfrage);
+        while($row = mysql_fetch_object($ergebnis))
+             {
+            $id =("$row->id");
+             }
+        $loeschen = "DELETE FROM timer WHERE id = '$id'";
+        $loesch = mysql_query($loeschen);
+        $reset = "0";
+        }
+    exec("sudo /home/pi/radiobeere/rb-timer-update.py");
+    ?>
 
-<div data-role="page" class="ui-responsive-panel" id="panel" data-title="RadioBeere">
-
+    <div data-role="page"
+         class="ui-responsive-panel"
+         id="panel"
+         data-title="RadioBeere">
         <div data-role="header">
-                <a href="#nav-panel" data-icon="bars" data-iconpos="notext">Men&uuml;</a>
-                <h1>RadioBeere</h1>
-                <a href="/" data-icon="home" data-iconpos="notext">Startseite</a>
+            <a href="#nav-panel"
+                 data-icon="bars"
+                 data-iconpos="notext">Menü</a>
+            <h1>RadioBeere</h1>
+            <a href="/"
+                 data-icon="home"
+                 data-iconpos="notext">Startseite</a>
         </div>
 
-<!--- Seiteninhalt --->
+        <div data-role="main"
+             class="ui-content">
+            <h2>Aufnahme planen</h2>
 
-        <div data-role="main" class="ui-content">
-	<h2>Aufnahme planen</h2>
+            <p>Was möchtest du aufnehmen?</p>
 
-	<p>Was m&ouml;chtest du aufnehmen?</p>
+            <div class="ui-field-contain">
+                <form action="record2.php"
+                      method="post">
+                    <select name="alias"
+                          onchange=
+                          "if(this.value != 0) { this.form.submit(); }">
+                        <option value="">
+                            Sender auswählen
+                        </option>
 
-<!--- Formular --->
+                        <?php
+                        $abfrage = "SELECT name,alias FROM sender ORDER BY name";
+                        $ergebnis = mysql_query($abfrage);
+                        while($row = mysql_fetch_object($ergebnis))
+                            {
+                            echo "<option value=\"$row->alias\">$row->name</option><br>";
+                            }
+                        ?>
 
-	<div class="ui-field-contain">
-	<form action="record2.php" method="post">
-        <select name="alias" onchange="if(this.value != 0) { this.form.submit(); }">
-	<option value="">Sender ausw&auml;hlen</option>
+                    </select>
+                </form>
+            </div>
 
-	<?php
-	$abfrage = "SELECT name,alias FROM sender ORDER BY name";
-	$ergebnis = mysql_query($abfrage);
-
-	while($row = mysql_fetch_object($ergebnis))
-   	{
-	echo "<option value=\"$row->alias\">$row->name</option><br>";
-   	}
-	?>
-
-	</select>
-	</form>
+            <div class="illu-content-wrapper">
+                <div class="illu-content illu-record">
+                </div>
+            </div>
         </div>
-
-
-       <div class="illu-content-wrapper">
-        <div class="illu-content illu-record">
-        </div></div>
-
-</div>
-
-<!--- Navigation --->
-
         <?php
         include("include/navigation.php");
         ?>
-
-</div>
-
-        <?php
-        include("include/jquery.php");
-        ?>
-
+    </div>
+    <?php
+    include("include/jquery.php");
+    ?>
 </body>
-
 </html>
